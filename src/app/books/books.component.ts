82b3f9ebb2698439307b756450bfb942638f6443
JD <janet_dornan@hotmail.com>
2023-08-19 15:09:00 +0100

    Add styling for book component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -4,6 +4,7 @@ interface Book {
   name: string;
   author: string;
   image: string;
+  amount: number;
 }
 
 @Component({
@@ -15,14 +16,16 @@ export class BooksComponent implements OnInit {
 
   books: Book[] = [
     {
-      name: 'Clean Code',
-      author: 'Robert C Martin',
+      name: 'clean code',
+      author: 'robert c martin',
       image: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+      amount: 25,
     },
     {
-      name: 'Pragmatic Programmer',
-      author: 'David Thomas',
+      name: 'pragmatic programmer',
+      author: 'david thomas',
       image: 'https://m.media-amazon.com/images/P/B07VRS84D1.01._SCLZZZZZZZ_SX500_.jpg',
+      amount: 30,
     },
   ]
 
